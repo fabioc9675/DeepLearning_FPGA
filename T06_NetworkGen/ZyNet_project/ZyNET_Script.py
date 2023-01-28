0a0ba1a3b33680ba829bd49a4019689674bf3e15
fabioc9675 <fabian.castano@udea.edu.co>
2023-01-28 11:35:10 -0500

    Changes in file project generator, also included support fo IP CORE

---
+++ T06_NetworkGen/ZyNet_project/ZyNET_Script.py
@@ -0,0 +1,25 @@
+from zynet import zynet
+from zynet import utils
+import numpy as np
+
+
+def genMnistZynet(dataWidth, sigmoidSize, weightIntSize, inputIntSize):
+    model = zynet.model()
+    # capa de entrada, representa el tamaño de la imagen como in vector flatten
+    model.add(zynet.layer("flatten", 784))
+    model.add(zynet.layer("Dense", 30, "sigmoid"))
+    model.add(zynet.layer("Dense", 30, "sigmoid"))
+    model.add(zynet.layer("Dense", 10, "sigmoid"))
+    model.add(zynet.layer("Dense", 10, "sigmoid"))
+    # Capa de salida, es un hardmax de la clasificacion
+    model.add(zynet.layer("Dense", 10, "hardmax"))
+    weightArray = utils.genWeightArray("weightsandbiases.txt")
+    biasArray = utils.genBiasArray("weightsandbiases.txt")
+    model.compile(pretrained='Yes', weights=weightArray, biases=biasArray, dataWidth=dataWidth,
+                  weightIntSize=weightIntSize, inputIntSize=inputIntSize, sigmoidSize=sigmoidSize)
+    zynet.makeXilinxProject('NN_Zynet_Prj', 'xc7z020clg400-1')
+    zynet.makeIP('NN_Zynet_Prj')
+    zynet.makeSystem('NN_Zynet_Prj', 'NN_Zinet_Block')
+
+
+genMnistZynet(dataWidth=8, sigmoidSize=10, weightIntSize=4, inputIntSize=1)
