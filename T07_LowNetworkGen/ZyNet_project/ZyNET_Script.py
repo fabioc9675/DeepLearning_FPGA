1f9d51d7e5fa1413445f8be6d0341ace8a2baa05
fabioc9675 <fabian.castano@udea.edu.co>
2023-02-05 00:39:50 -0500

    Modification in source script to generate Zynet Prj

--- T07_LowNetworkGen/ZyNet_project/ZyNET_Script.py
+++ T07_LowNetworkGen/ZyNet_project/ZyNET_Script.py
@@ -8,8 +8,7 @@ def genMnistZynet(dataWidth, sigmoidSize, weightIntSize, inputIntSize):
     # capa de entrada, representa el tamaño de la imagen como in vector flatten
     model.add(zynet.layer("flatten", 784))
     model.add(zynet.layer("Dense", 30, "sigmoid"))
-    model.add(zynet.layer("Dense", 30, "sigmoid"))
-    model.add(zynet.layer("Dense", 10, "sigmoid"))
+    model.add(zynet.layer("Dense", 20, "sigmoid"))
     model.add(zynet.layer("Dense", 10, "sigmoid"))
     # Capa de salida, es un hardmax de la clasificacion
     model.add(zynet.layer("Dense", 10, "hardmax"))
@@ -19,7 +18,7 @@ def genMnistZynet(dataWidth, sigmoidSize, weightIntSize, inputIntSize):
                   weightIntSize=weightIntSize, inputIntSize=inputIntSize, sigmoidSize=sigmoidSize)
     zynet.makeXilinxProject('NN_Zynet_Prj', 'xc7z020clg400-1')
     zynet.makeIP('NN_Zynet_Prj')
-    zynet.makeSystem('NN_Zynet_Prj', 'NN_Zinet_Block')
+    zynet.makeSystem('NN_Zynet_Prj', 'NN_Zynet_Block')
 
 
-genMnistZynet(dataWidth=16, sigmoidSize=10, weightIntSize=4, inputIntSize=1)
+genMnistZynet(dataWidth=8, sigmoidSize=10, weightIntSize=4, inputIntSize=1)
